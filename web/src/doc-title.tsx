cdfc01708eda19f820ee4463ad6e56892e7d589a
mac13 <mac13@mac13.local>
2023-11-08 08:58:42 +0300

    mixpanel

--- web/src/doc-title.tsx
+++ web/src/doc-title.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
+import mixpanel from 'mixpanel-browser';
 
 export const useDocumentTitle = (title: string) => {
   useEffect(() => {
     document.title = 'Motkhss |  المتخصص | ' + title;
+    mixpanel.track(title);
   }, [title]);
 };
